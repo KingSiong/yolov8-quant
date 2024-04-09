94290da661d731f8bceb5147a7237324ee98e501
KingSiong <kingsiong@163.com>
2024-04-09 12:26:59 +0800

    add new utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import torch
 import os
 from tqdm import tqdm
+from copy import deepcopy
+from ultralytics.utils.torch_utils import de_parallel
 
 from pytorch_quantization import nn as quant_nn
 from pytorch_quantization import calib
@@ -68,25 +70,25 @@ def calibrate_model(model, model_name, data_loader, num_calib_batch, calibrator,
 
         if not calibrator == "histogram":
             compute_amax(model, method="max")
-            calib_output = os.path.join(out_dir, F"{model_name}-max-{num_calib_batch * data_loader.batch_size}.pth")
-            ckpt = {'model': model}
-            torch.save(ckpt, calib_output)
+            # calib_output = os.path.join(out_dir, F"{model_name}-max-{num_calib_batch * data_loader.batch_size}.pth")
+            # ckpt = {'model': model}
+            # torch.save(ckpt, calib_output)
         else:
             for percentile in hist_percentile:
                 print(F"{percentile} percentile calibration")
                 compute_amax(model, method="percentile")
-                calib_output = os.path.join(out_dir, F"{model_name}-percentile-{percentile}-{num_calib_batch * data_loader.batch_size}.pth")
+                # calib_output = os.path.join(out_dir, F"{model_name}-percentile-{percentile}-{num_calib_batch * data_loader.batch_size}.pth")
                 # torch.save(model.state_dict(), calib_output)
-                ckpt = {'model': model}
-                torch.save(ckpt, calib_output)
+                # ckpt = {'model': model}
+                # torch.save(ckpt, calib_output)
 
             for method in ["mse", "entropy"]:
                 print(F"{method} calibration")
                 compute_amax(model, method=method)
-                calib_output = os.path.join(out_dir, F"{model_name}-{method}-{num_calib_batch * data_loader.batch_size}.pth")
+                # calib_output = os.path.join(out_dir, F"{model_name}-{method}-{num_calib_batch * data_loader.batch_size}.pth")
                 # torch.save(model.state_dict(), calib_output)
-                ckpt = {'model': model}
-                torch.save(ckpt, calib_output)
+                # ckpt = {'model': model}
+                # torch.save(ckpt, calib_output)
 
 class disable_quantization:
     def __init__(self, model):
@@ -102,3 +104,26 @@ class disable_quantization:
 
     def __exit__(self, *args, **kwargs):
         self.apply(False)
+
+class enable_quantization:
+    def __init__(self, model):
+        self.model = model
+    
+    def apply(self, enabled=True):
+        for name, module in self.model.named_modules():
+            if isinstance(module, quant_nn.TensorQuantizer):
+                module._disabled = not enabled
+            
+    def __enter__(self):
+        self.apply(enabled=True)
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.apply(enabled=False)
+
+def save_model(model, path):
+    ckpt = {
+        "model": deepcopy(de_parallel(model)),
+    }
+
+    torch.save(ckpt, path)
